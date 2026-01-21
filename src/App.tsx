532a0dab97dfa99f65330ec3fc917234449ba595
Henry Alicoben <dominicalicoben2016@gmail.com>
2026-01-21 15:48:03 +0800

    Update React Router and remove unused dependencies
    Upgraded react-router and react-router-dom to latest versions and updated @remix-run/router. Removed unused dependencies including @emailjs/browser and tempo-devtools. Added .npmrc with shamefully-hoist for improved package management. Updated Vite and main app files to support new router versions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
-import { Toaster} from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster";
 import Home from "./components/home";
-import routes from "tempo-routes";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
         <Toaster />
-        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
       </>
     </Suspense>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import emailjs from '@emailjs/browser';
-
-import { TempoDevtools } from "tempo-devtools";
-TempoDevtools.init();
-
-emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
 
 const basename = import.meta.env.BASE_URL;
 
